3a80a9d59d41c49cf8a5cb99cf5534bf5dcb6307
LauraVillegas <vils.in.town@gmail.com>
2023-05-19 14:37:40 -0700

    first commit

---
+++ components/layout/layout.js
@@ -0,0 +1,67 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from './layout.module.css';
+import utilStyles from '../../styles/utils.module.css';
+import Link from 'next/link';
+
+const name = 'Lau Villegas';
+export const siteTitle = 'Lau\'s Home Site';
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Lau Villegas' Personal Site. Lau is a passionate and experienced Software Engineer who enjoys solving hard problems through code. This is a blog about tech, projects, ideas, art, life updates or anything else Lau is obsessing over."
+        />
+        <meta
+          property="og:image"
+          content={`https://drive.google.com/uc?export=view&id=1D3IOWI_b0GFGVieBqnHlkwHwXgeDXmJR`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <Image
+              priority
+              src="/images/profile.png"
+              className={utilStyles.borderCircle}
+              height={144}
+              width={108}
+              alt=""
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <Image
+                priority
+                src="/images/profile.png"
+                className={utilStyles.borderCircle}
+                height={144}
+                width={108}
+                alt=""
+              />
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/" className={utilStyles.colorInherit}>
+                {name}
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">← Back to home</Link>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,44 @@
+import Head from 'next/head';
+import { getSortedPostsData } from '../lib/posts';
+import Layout, { siteTitle } from '../components/layout/layout';
+import Link from 'next/link';
+import Date from '../components/date/date';
+import utilStyles from '../styles/utils.module.css';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hey there! I'm Lau. A creative software engineer and idea generator who is obsessed with solving hard human problems and building the future.</p>
+        <p>I've built this site with the intention of sharing some of my projects, writings, ideas and anything in between.</p>
+        <p>If you'd like to learn how to develop and deploy a site like this for free.50, I recommend you check out these next.js tutorials on <Link href={'https://nextjs.org/learn'}>getting started</Link> and <Link href={'https://nextjs.org/docs/pages/building-your-application/deploying'}>deployment</Link>. You can also find all the code for this site on <Link href={'https://github.com/ViLaud/LauVillegas'}>it's github repo</Link>. Good luck!</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
